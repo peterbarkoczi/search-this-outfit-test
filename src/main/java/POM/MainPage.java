258c29827a42726a7c326705c6efed55d10a4aa4
peterbarkoczi <barkopetya@gmail.com>
2020-04-29 22:33:23 +0200

    implement test: Navigate To Main Page

---
+++ src/main/java/POM/MainPage.java
@@ -0,0 +1,25 @@
+package POM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class MainPage extends BasePage {
+
+    @FindBy(className = "title-a")
+    private WebElement logo;
+
+    public MainPage(WebDriver driver) {
+        super(driver);
+        this.url = "http://" + ip + ":" + port + "/";
+}
+
+    public void navigateToMainPage() {
+        navigateTo(url);
+    }
+
+    public boolean logoIsAppeared() {
+        return wait.until(ExpectedConditions.visibilityOf(logo)).isDisplayed();
+    }
+}
