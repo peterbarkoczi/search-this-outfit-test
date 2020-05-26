ecf50e5f7018bd4b62b18de1a94bd777547aaf1f
peterbarkoczi <barkopetya@gmail.com>
2020-05-26 09:39:15 +0200

    test#14

--- src/test/java/searchthisoutfit/Hook.java
+++ src/test/java/searchthisoutfit/Hook.java
@@ -19,7 +19,7 @@ public class Hook {
     @Before
     public void setupDriver() {
         try {
-            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub/"), new ChromeOptions());
+            driver = new RemoteWebDriver(new URL("http://ip-172-17-0-2.eu-west-1.compute.internal:4444/wd/hub/"), new ChromeOptions());
         } catch (MalformedURLException e) {
             System.out.println("The remote webdriver could not connect");
             driver = DriverFactory.getDriver();
